6b8550cbb18971405ee1248400ab82fb284ba2de
up202007253 <up202007253@g.uporto.pt>
2024-10-18 13:21:45 +0100

    llopen tentativa agora tem a maquina de estados

--- RC_code/src/link_layer.c
+++ RC_code/src/link_layer.c
@@ -17,6 +17,14 @@
 #define TRANSMITTER 0
 #define RECEIVER 1
 
+// Definição dos estados 
+#define START 0
+#define FLAG_RECEIVED 1
+#define A_RECEIVED 2
+#define C_RECEIVED 3
+#define BCC_VALID 4
+#define STOP 5
+
 typedef struct {
     unsigned char FLAG;
     unsigned char A_TRANSMISSOR;
@@ -31,108 +39,222 @@ typedef struct {
 Protocolo protocolo = {0x7E, 0x03, 0x01, 0x03, 0x07, 0x05, 0x01};
 
 int fd;
-int alarmEnabled = 0;
+int alarmEnabled = FALSE;
 int alarmCount = 0;
+
 
 // Manipulador do alarme
 void alarmHandler(int signal) {
-    alarmEnabled = 0;
+    alarmEnabled = FALSE;
     alarmCount++;
     printf("Alarme #%d\n", alarmCount);
 }
-////////////////////////////////////////////////
-// LLOPEN
-////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters){
 
+// Função para configurar a porta serial
+int configurePort(LinkLayer connectionParameters) {
     struct termios oldtio, newtio;
-    unsigned char buffer[BUFFER_SIZE];
-
-    // Abrir o serial port
+    
+    // Abrir a porta serial
     fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
     if (fd < 0) {
         perror("Erro ao abrir a porta serial");
         return -1;
     }
 
-    // Guardar a configuração anterior e configurar a porta
+    // Obter a configuração atual da porta e guardar em oldtio
     if (tcgetattr(fd, &oldtio) == -1) {
         perror("Erro ao obter a configuração da porta");
         return -1;
     }
-    
+    // Clear struct for new port settings
+    // Configurar a nova estrutura para definir a porta
     memset(&newtio, 0, sizeof(newtio));
+
+
     newtio.c_cflag = connectionParameters.baudRate | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
+
+    // Set input mode
     newtio.c_lflag = 0;
     newtio.c_cc[VTIME] = 1;
     newtio.c_cc[VMIN] = 0;
 
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+
+    // Limpar o buffer da porta e aplicar a nova configuração
     tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
     if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
         perror("Erro ao configurar a porta");
         return -1;
     }
 
+    return 0;
+}
+////////////////////////////////////////////////
+// LLOPEN
+////////////////////////////////////////////////
+int llopen(LinkLayer connectionParameters){
+
+    unsigned char buffer[BUFFER_SIZE];
+
+    // Configuração da porta serial usando configurePort
+    if (configurePort(connectionParameters) < 0) {
+        return -1;
+    }
+   
     // Configuração do alarme
     (void) signal(SIGALRM, alarmHandler);
 
     if (connectionParameters.role == TRANSMITTER) {
         // Construir e enviar a trama SET
         unsigned char setFrame[5] = {protocolo.FLAG, protocolo.A_TRANSMISSOR, protocolo.CTRL_SET, protocolo.A_TRANSMISSOR ^ protocolo.CTRL_SET, protocolo.FLAG};
-        
-        while (alarmCount < connectionParameters.nRetransmissions) {
-            // Enviar a trama SET
+        // Máquina de estados para processar a resposta UA
+        int currentState = START;
+        int res;
+        // Retransmissão com base no número de tentativas
+        for (int i = 0; i < connectionParameters.nRetransmissions; i++) {
+            // Reiniciar alarmEnabled antes de enviar novamente
+            alarmEnabled = FALSE;
             write(fd, setFrame, sizeof(setFrame));
-            printf("A enviar a trama SET\n");
+            printf("Trama SET enviada (Tentativa %d)\n", i + 1);
+
 
             // Ativar o alarme
-            alarmEnabled = 1;
+            alarmEnabled = TRUE;
             alarm(connectionParameters.timeout);
 
-            // Esperar pela resposta UA
-            while (alarmEnabled) {
-                int res = read(fd, buffer, BUFFER_SIZE); // Ler para o buffer de 256 bytes
-                if (res >= 5 && buffer[0] == protocolo.FLAG && buffer[1] == protocolo.A_RECEPTOR && 
-                    buffer[2] == protocolo.CTRL_UA && buffer[3] == (protocolo.A_RECEPTOR ^ protocolo.CTRL_UA) && buffer[4] == protocolo.FLAG) {
-                    printf("Trama UA recebida\n");
-                    alarm(0);
-                    tcsetattr(fd, TCSANOW, &oldtio);
-                    return fd;
+            // Processar a resposta UA utilizando a máquina de estados
+            while (alarmEnabled && currentState != STOP) {
+                unsigned char byte = 0;
+                res = read(fd, &byte, sizeof(byte));
+                if (res < 0) {
+                    perror("Erro ao ler a trama");
+                    return -1;
+                }
+                                // Atualizar o estado com base no byte recebido
+                if (res > 0) {
+                    switch (currentState) {
+                        case START:
+                            printf("Estado: START\n");
+                            if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                            break;
+                        case FLAG_RECEIVED:
+                            printf("Estado: FLAG_RECEIVED\n");
+                            if (byte == protocolo.A_RECEPTOR) currentState = A_RECEIVED;
+                            else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                            else currentState = START;
+                            break;
+                        case A_RECEIVED:
+                            printf("Estado: A_RECEIVED\n");
+                            if (byte == protocolo.CTRL_UA) currentState = C_RECEIVED;
+                            else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                            else currentState = START;
+                            break;
+                        case C_RECEIVED:
+                            printf("Estado: C_RECEIVED\n");
+                            if (byte == (protocolo.A_RECEPTOR ^ protocolo.CTRL_UA)) currentState = BCC_VALID;
+                            else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                            else currentState = START;
+                            break;
+                        case BCC_VALID:
+                            printf("Estado: BCC_VALID\n");
+                            if (byte == protocolo.FLAG) {
+                                currentState = STOP;
+                            } else {
+                                currentState = START;
+                            }
+                            break;
+                        default:
+                            printf("Estado: DEFAULT\n");
+                            currentState = START;
+                            break;
+                    }
                 }
             }
-
-            printf("Não foi recebida a trama UA, a retransmitir...\n");
+                
+            // Verificar se a trama UA foi recebida corretamente
+            if (currentState == STOP) {
+                printf("Trama UA recebida corretamente!\n");
+                alarm(0);  // Desativar o alarme
+                alarmEnabled = FALSE;  // Reiniciar alarmEnabled
+                return fd;
+            } else {
+                printf("Não foi recebida a trama UA, a retransmitir...\n");
+                currentState = START; // Reiniciar a máquina de estados para a próxima tentativa
+                alarmEnabled = FALSE;
+            }
         }
 
-        // Se o número máximo de tentativas for atingido
-        printf("Erro: não foi recebida a trama UA após várias tentativas\n");
-        tcsetattr(fd, TCSANOW, &oldtio);
+        printf("Erro: não foi possível estabelecer a conexão após várias tentativas\n");
         return -1;
-
-    } else if (connectionParameters.role == RECEIVER) {
-        // Esperar pela trama SET
+    }
+    else if (connectionParameters.role == RECEIVER) {
+        int currentState = START;
         while (1) {
-            int res = read(fd, buffer, BUFFER_SIZE); // Ler para o buffer de 256 bytes
-            if (res >= 5 && buffer[0] == protocolo.FLAG && buffer[1] == protocolo.A_TRANSMISSOR && buffer[2] == protocolo.CTRL_SET && buffer[3] == (protocolo.A_TRANSMISSOR ^ protocolo.CTRL_SET) && buffer[4] == protocolo.FLAG) {
-                printf("Trama SET recebida\n");
+            unsigned char byte = 0;
+            int res = read(fd, &byte, sizeof(byte));
+            if (res < 0) {
+                perror("Erro ao ler a trama");
+                return -1;
+            }
 
-                // Construir e enviar a trama UA
-                unsigned char uaFrame[5] = {protocolo.FLAG, protocolo.A_RECEPTOR, protocolo.CTRL_UA, protocolo.A_RECEPTOR ^ protocolo.CTRL_UA, protocolo.FLAG};
-                write(fd, uaFrame, sizeof(uaFrame));
-                printf("A enviar a trama UA\n");
-
-                tcsetattr(fd, TCSANOW, &oldtio);
-                return fd;
+            // Atualizar o estado com base no byte recebido
+            if (res > 0) {
+                switch (currentState) {
+                    case START:
+                        printf("Estado: START\n");
+                        if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                        break;
+                    case FLAG_RECEIVED:
+                        printf("Estado: FLAG_RECEIVED\n");
+                        if (byte == protocolo.A_TRANSMISSOR) currentState = A_RECEIVED;
+                        else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                        else currentState = START;
+                        break;
+                    case A_RECEIVED:
+                        printf("Estado: A_RECEIVED\n");
+                        if (byte == protocolo.CTRL_SET) currentState = C_RECEIVED;
+                        else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                        else currentState = START;
+                        break;
+                    case C_RECEIVED:
+                        printf("Estado: C_RECEIVED\n");
+                        if (byte == (protocolo.A_TRANSMISSOR ^ protocolo.CTRL_SET)) currentState = BCC_VALID;
+                        else if (byte == protocolo.FLAG) currentState = FLAG_RECEIVED;
+                        else currentState = START;
+                        break;
+                    case BCC_VALID:
+                        printf("Estado: BCC_VALID\n");
+                        if (byte == protocolo.FLAG) {
+                            currentState = STOP;
+                            unsigned char uaFrame[5] = {protocolo.FLAG, protocolo.A_RECEPTOR, protocolo.CTRL_UA, protocolo.A_RECEPTOR ^ protocolo.CTRL_UA, protocolo.FLAG};
+                            write(fd, uaFrame, sizeof(uaFrame));
+                            printf("Trama UA enviada\n");
+                            return fd;
+                        } else {
+                            currentState = START;
+                        }
+                        break;
+                    default:
+                        printf("Estado: DEFAULT\n");
+                        currentState = START;
+                        break;
+                }
             }
         }
     }
 
-    // Restaurar a configuração original se algo falhar
-    tcsetattr(fd, TCSANOW, &oldtio);
     return -1;
-
     /*if (openSerialPort(connectionParameters.serialPort,
                        connectionParameters.baudRate) < 0)
     {
